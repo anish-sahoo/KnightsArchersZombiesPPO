c405c74ba8244ecad0228e4b8fd45d3981cd3b96
Anish Sahoo <anishsahoo2005@gmail.com>
2024-11-26 15:23:54 -0500

    Add test bench

--- test.py
+++ test.py
@@ -2,18 +2,24 @@ from pettingzoo.butterfly import knights_archers_zombies_v10
 
 env = knights_archers_zombies_v10.parallel_env(
     render_mode=None,
-    vector_state=True,
-    num_archers=2,
-    num_knights=2,
-    max_zombies=10,
+    vector_state=False,
+    num_archers=4,
+    num_knights=4,
+    max_zombies=50,
 )
 observations, infos = env.reset()
-
+a = 0
+reward = 0
 while env.agents:
     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
     observations, rewards, terminations, truncations, infos = env.step(actions)
-    print(len(observations))
-    for key, obs in observations.items():
-        print(key, obs.shape)
-    break
+    sum_rewards = sum(rewards.values())
+    reward += sum_rewards
+    
+    if reward > 1 and sum_rewards > 0:
+        print(f"Step {a}: {reward}")
+    a += 1
+        
 env.close()
+
+print(reward)
